27039c84497f2cf2430e1032175a8b9986938809
Chris Lawrence <lawrenc@live.com>
2020-05-21 16:38:01 -0500

    Third commit- Added colors.xml file to style colors for main activity. Added Styles headings to simplify the customization of the views a made the app more streamlined. Implemented working onClickListeners for all the current colors.

--- app/src/main/java/com/example/android/mypal_ette/MainActivity.java
+++ app/src/main/java/com/example/android/mypal_ette/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.android.mypal_ette;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,17 +19,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        ImageButton colorChosen = (ImageButton) findViewById(R.id.base_brown);
-        colorChosen.setOnClickListener(new View.OnClickListener() {
+        //Pulling textviews and finding them by ID so they can be used for onclick Listener
+
+
+        final TextView baseBrownChosen = (TextView) findViewById(R.id.base_brown);
+        final TextView darkBrownChosen = (TextView) findViewById(R.id.dark_brown);
+        final TextView tanChosen = (TextView) findViewById(R.id.tan);
+        final TextView brickChosen = (TextView) findViewById(R.id.brick_color);
+        final TextView violetChosen = (TextView) findViewById(R.id.violet);
+        final TextView slateChosen = (TextView) findViewById(R.id.slate);
+
+
+        //Onlick listener Instructions for the colors
+        baseBrownChosen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pickedColor = "Red, yellow, brown, white(extra)";
                 displayColors(pickedColor);
 
-            }
 
+                ArrayList<TextView> colorChoice = new ArrayList<TextView>();
+                colorChoice.add((TextView) findViewById(R.id.base_brown));
+                colorChoice.add((TextView) findViewById(R.id.dark_brown));
+
+
+            darkBrownChosen.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pickedColor = "Base brown, burnt umber, blue";
+                    displayColors(pickedColor);
+
+
+                    tanChosen.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            pickedColor = "Red, white, yellow(extra)";
+                            displayColors(pickedColor);
+
+                            brickChosen.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    pickedColor = "Orange and violet";
+                                    displayColors(pickedColor);
+
+                                    violetChosen.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            pickedColor = "red and blue";
+                                            displayColors(pickedColor);
+
+                                            slateChosen.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    pickedColor = "violet and green";
+                                                    displayColors(pickedColor);
+                                                }
+                                            });
+
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+
+                }
+            });
+
+            }
         });
     }
+
+
         public void displayColors(String message) {
 
             TextView orderSummaryTextView = (TextView) findViewById(R.id.color_recommendations);
